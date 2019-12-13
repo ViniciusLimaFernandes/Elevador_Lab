47c880a644ec3e4f1868b1865bafe70561556e80
Vinicius Lima Fernandes <vfernandes@uolinc.com>
2019-12-13 12:18:25 -0300

    Revert "SW 16 no tb"
    This reverts commit c5bc74e4070cda0dbd687f945e1a3603c4b7cb63.

--- src/main/elevador.vhd
+++ src/main/elevador.vhd
@@ -77,18 +77,6 @@ ARCHITECTURE controlador OF elevador IS
 --		END IF;
 --	END PROCESS;
 
-WITH andar SELECT
-    segmentos <= "1111110" WHEN "0000",
-                 "0110000" WHEN "0001",
-                 "1101101" WHEN "0010",
-                 "1111001" WHEN "0011",
-                 "0110011" WHEN "0100",
-                 "1011011" WHEN "0101",
-                 "1011111" WHEN "0110",
-                 "1110000" WHEN "0111",
-                 "1111111" WHEN "1000",
-                 "1111011" WHEN "1001",
-                 "1111110" WHEN OTHERS;
 
 	--Logica dos botoes
 	PROCESS(clk)
